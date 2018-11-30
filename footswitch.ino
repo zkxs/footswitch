0329f924d1e48c504ed9fd204707b6e31c12bf51
Michael Ripley <zkxs00@gmail.com>
2018-11-29 23:28:31 -0600

    vscode integration

--- footswitch.ino
+++ footswitch.ino
@@ -20,7 +20,7 @@ void setup() {
   // put your setup code here, to run once:
   pinMode(buttonPin, INPUT_PULLUP);
   Keyboard.begin();
-//  Serial.begin(9600);
+//  Serial.begin(115200);
 }
 
 void loop() {
